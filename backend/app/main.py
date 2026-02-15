e2652c2e00ce4394c380ff8dfdfd991cd9601376
noranhc <noranhcam@gmail.com>
2026-02-15 01:49:55 -0500

    created circle endpoints some rules added that seem sensible

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import init_db
-from app.controllers import user_controller, auth_controller, me_controller, friend_request_controller
+from app.controllers import user_controller, auth_controller, me_controller, friend_request_controller, circle_controller
 import time
 from sqlalchemy import text
 from app.database import engine
@@ -26,6 +26,7 @@ app.include_router(auth_controller.router)
 app.include_router(user_controller.router, prefix="/users")
 app.include_router(me_controller.router)
 app.include_router(friend_request_controller.router)
+app.include_router(circle_controller.router)
 
 
 @app.on_event("startup")
