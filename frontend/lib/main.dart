5ddc407d45aa37e406ec54a3dd0696ad458fa6f2
Nora Cam <121891599+noranhc@users.noreply.github.com>
2026-02-15 11:10:39 -0500

    Revert "rename"

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'OutSource',
+      title: 'Aura',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
